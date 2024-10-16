fbe2e5f5a347787002ce1a9994837887bb8c5bd3
Frank Mwito <Mfranklyne039@gmail.com>
2024-10-16 09:11:06 +0300

    string reversal

---
+++ string reversal.py
@@ -0,0 +1,11 @@
+# reverse a given string
+
+def reverse_string(name: str):
+    for character in name[::-1]:
+        print(character)
+
+
+
+reverse_string(input("Enter any string choice: "))
+        
+        
